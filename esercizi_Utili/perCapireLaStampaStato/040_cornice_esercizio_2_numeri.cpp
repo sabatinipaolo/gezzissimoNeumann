05287338b7403cafe41b8002e84b1bf0e4968020
paolo <paolo@foo.com>
2023-10-24 22:26:26 +0200

    aggiunti esercizi

---
+++ esercizi_Utili/perCapireLaStampaStato/040_cornice_esercizio_2_numeri.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int primo_numero, secondo_numero;
+    cout << "inserire un numero intero ";
+    cin >> primo_numero;
+    cout << "inserire un altro numero intero ";
+    cin >> secondo_numero;
+
+    // TODO : fare quanto richiesto ..
+}
---
+++ esercizi_Utili/perCapireLaStampaStato/050_cornice_esercizio_tabellina_di_n.cpp
@@ -0,0 +1,40 @@
+/* Esercizio:
+    stampare la tabellina (sino alla decima ) del numero inserito dall'utente
+    esempio
+    se l'utente inserisce il numero 7 il programma stampa:
+
+    +------------+
+    |          7 |
+    +------------+
+    |         14 |
+    +------------+
+    |         21 |
+    +------------+
+    |         28 |
+    +------------+
+    |         35 |
+    +------------+
+    |         42 |
+    +------------+
+    |         49 |
+    +------------+
+    |         56 |
+    +------------+
+    |         63 |
+    +------------+
+    |         70 |
+    +------------+
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int numero;
+    cout << "inserire un numero intero ";
+    cin >> numero;
+
+    // TODO : fare quanto richiesto ..
+}
